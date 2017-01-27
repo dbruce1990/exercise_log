6254d94f66df82eb529f3b41bed013147acf62a3
dustin bruce <dustin bruce>
2017-01-27 12:59:43 -0800

    cleaning up Calendar.jsx

--- react-components/Calendar.jsx
+++ react-components/Calendar.jsx
@@ -7,29 +7,21 @@ class Calendar extends Component {
             date: new Date(),
             days: [],
             weeks: [],
-            monthInput: new Date().getMonth(),
-            yearInput: new Date().getFullYear(),
             month: new Date().getMonth(),
-            year: new Date().getFullYear()
+            year: new Date().getFullYear(),
+            daysOfWeek: [
+                "Sunday",
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday"
+            ]
         }
 
         this.getDaysInMonth = this
             .getDaysInMonth
-            .bind(this);
-        this.displayDay = this
-            .displayDay
-            .bind(this);
-        this.getWeeks = this
-            .getWeeks
-            .bind(this);
-        this.displayWeek = this
-            .displayWeek
-            .bind(this);
-        this.monthInputOnChange = this
-            .monthInputOnChange
-            .bind(this);
-        this.yearInputOnChange = this
-            .yearInputOnChange
             .bind(this);
         this.displayMonthName = this
             .displayMonthName
@@ -60,38 +52,6 @@ class Calendar extends Component {
             this.setState({
                 month: this.state.month + 1
             })
-    }
-
-    monthInputOnChange(e) {
-        this.setState({
-            month: e.target.value - 1
-        })
-    }
-
-    yearInputOnChange(e) {
-        this.setState({year: e.target.value})
-    }
-
-    componentWillMount() {
-        this.setState({
-            days: this.getDaysInMonth()
-        }, () => this.setState({
-            weeks: this.getWeeks(this.state.days)
-        }))
-    }
-
-    displayDay(day) {
-        return (
-            <div className="calendar__day--highlight mdl-cell mdl-cell--1-col">{day.getDate()}</div>
-        )
-    }
-
-    displayWeek(week) {
-        return (
-            <div className="mdl-grid">
-                {week.map(day => this.displayDay(day))}
-            </div>
-        )
     }
 
     displayMonthName() {
@@ -139,96 +99,104 @@ class Calendar extends Component {
         }
         return (
             <div className="mdl-grid">
-                <div className="mdl-cell mdl-cell--1-col">
-                    <button
-                        className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect"
-                        onClick={this.previousMonth}>
-                        <i className="material-icons">remove</i>
-                    </button>
+                <div className="mdl-cell mdl-cell--1-col" onClick={this.previousMonth}>
+                    <i className="material-icons">chevron_left</i>
                 </div>
 
                 <div className="mdl-cell mdl-cell--4-col">
                     <h2>{month}</h2>
                 </div>
 
-                <div className="mdl-cell mdl-cell--1-col">
-                    <button
-                        className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-js-ripple-effect"
-                        onClick={this.nextMonth}>
-                        <i className="material-icons">add</i>
-                    </button>
+                <div className="mdl-cell mdl-cell--1-col" onClick={this.nextMonth}>
+                    <i className="material-icons">chevron_right</i>
                 </div>
 
             </div>
         )
     }
 
-    getWeeks(days) {
-        const divStyle = {
-            border: "1px solid black"
-        }
-
-        const weeks = [
-            [], [], [], [], []
-        ]
-        days.forEach(day => {
-            if (day.getDate() <= 7) {
-                weeks[0].push(day)
-            } else if (day.getDate() <= 14) {
-                weeks[1].push(day)
-            } else if (day.getDate() <= 21) {
-                weeks[2].push(day)
-            } else if (day.getDate() <= 28) {
-                weeks[3].push(day)
-            } else {
-                weeks[4].push(day)
-            }
-        })
-        return weeks
-    }
-
     getDaysInMonth() {
-        const date = new Date(this.state.date.getFullYear(), this.state.date.getMonth(), 1)
+        const date = new Date(this.state.year, this.state.month, 1)
         const days = []
-        while (date.getMonth() === this.state.date.getMonth()) {
+        while (date.getMonth() === this.state.month) {
             days.push(new Date(date))
             date.setDate(date.getDate() + 1)
         }
         return days
     }
 
+    displayMonth() {
+        const daysInMonth = this.getDaysInMonth()
+        let month = []
+
+        let week = 0
+        let currentWeek = [
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null
+        ]
+
+        daysInMonth.forEach(today => {
+            const dayOfWeek = today.getDay()
+            const todaysDate = today.getDate()
+            const isSaturday = () => today.getDay() == 6
+            const lastDayOfMonth = () => daysInMonth.length == todaysDate
+
+            currentWeek[dayOfWeek] = today
+
+            if (isSaturday()) {
+                month[week] = currentWeek
+                currentWeek = [
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null
+                ]
+                week += 1
+            }
+
+            if (lastDayOfMonth()) {
+                month[week] = currentWeek
+            }
+
+        })
+
+        const renderDay = (day, i) => day !== null
+            ? <td key={"day" + i}>{day.getDate()}</td>
+            : <td key={"empty" + i}>empty</td>
+
+        const renderWeek = (week, i) => <tr key={"week" + i}>{week.map((day, i) => renderDay(day, i))}</tr>
+        
+
+        month = month.map((week, i) => renderWeek(week, i))
+        return month
+    }
+
     render() {
         return (
             <div>
-                <h1>Calendar</h1>
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <label htmlFor="calendar_month" className="mdl-textfield__label">Month</label>
-                    <input
-                        className="mdl-textfield__input"
-                        type="number"
-                        id="calendar_month"
-                        onChange={this.monthInputOnChange}/>
-                    <span className="mdl-textfield__error">Enter a number between 1-12.</span>
-                </div>
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <label htmlFor="calendar_year" className="mdl-textfield__label">Year</label>
-                    <input
-                        className="mdl-textfield__input"
-                        type="number"
-                        id="calendar_year"
-                        onChange={this.yearInputOnChange}/>
-                </div>
-
                 <div>{this.displayMonthName()}</div>
-
-                <div>
-                    <p>{this.state.month}</p>
-                    <p>{this.state.year}</p>
-                </div>
-                {this
-                    .state
-                    .weeks
-                    .map(week => this.displayWeek(week))}
+                <table
+                    style={{
+                    border: "1px solid black"
+                }}>
+                    <tbody>
+                        <tr>
+                            {this
+                                .state
+                                .daysOfWeek
+                                .map((day, i) => <th key={i}>{day}</th>)}
+                        </tr>
+                        {this.displayMonth()}
+                    </tbody>
+                </table>
             </div>
         )
     }
